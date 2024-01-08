48a6d3d388efb5cf00d509ed245a7364886617c2
Akshay Kumar <akshayjnp2018@gmail.com>
2024-01-09 02:11:27 +0530

    vite_react_app

---
+++ 01vitereactapp/src/App.jsx
@@ -0,0 +1,19 @@
+
+import Render from "./chai"     // import just like app.jsx in main.jsx
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>First React App Using Vite</h1>
+        <Render />
+        
+      </div>
+
+      <h1>Vite + React</h1>
+      
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereactapp/src/chai.jsx
@@ -0,0 +1,10 @@
+function Render(){
+    return (
+
+        <h3>
+            chai.jsx file is rending in app.jsx
+        </h3>
+    )
+    
+}
+export default Render       // exoort just like app.jsx
