b9f99c079918e9ba1b649593ae4aa527bc4c1828
Akshay Kumar <akshayjnp2018@gmail.com>
2024-01-09 02:09:01 +0530

    create_react_app

---
+++ 01basicreactapp/src/App.js
@@ -0,0 +1,13 @@
+import Render from "./chai";
+
+function App() {
+  return (
+    
+    <div className="App">
+      <h1>First React Project Using create-react-app</h1>
+      <Render />
+    </div>
+  );
+}
+
+export default App;
---
+++ 01basicreactapp/src/chai.js
@@ -0,0 +1,7 @@
+function Render(){
+    return (
+        <h2>chai.js is rending in app.js</h2>
+    )
+}
+
+export default Render
